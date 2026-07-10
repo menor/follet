dc13919bcc352e4162dd7c4c2b1cc8d610ac2885
menor <github@menor.pro>
2026-07-10 06:36:46 +0200

    export standalone runToCheck function

--- main.ts
+++ main.ts
@@ -620,6 +620,14 @@ function render(messages: Message[], from: number) {
   }
 }
 
+export async function runToCheckpoint(initial: AgentState): Promise<AgentState> {
+  let state = initial
+  while (initial.status === "thinking") {
+    state = await runStep(state)
+  }
+  return state
+}
+
 async function drive(
   state: AgentState,
   shownFrom: number,
